50b1f8322e7d8787ab7a5f6d1e82c5740f9609b3
heshuai <heshuai@300.cn>
2021-04-16 19:10:02 +0800

    fix:123

--- src/pages/home/index/index.jsx
+++ src/pages/home/index/index.jsx
@@ -1,10 +1,21 @@
-import { Component } from 'react'
-import { View, Text, Swiper, SwiperItem } from '@tarojs/components'
+import { Component, useState} from 'react'
+import { View, Text, Swiper, SwiperItem, Image } from '@tarojs/components'
 import { } from 'taro-ui'
 
 import './index.scss'
 
+const swiperChange = (e) =>{
+    console.log(e.detail.current)
+}
+
 export default class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      swiperCurrent: 0
+    };
+  }
 
   componentWillMount() { }
 
@@ -36,11 +47,60 @@ export default class Index extends Component {
           </View>
         </View>
         <View className='menuTransform'>
-          <Swiper autoplay>
+          <Swiper autoplay circular className='swiperContent' onChange={swiperChange}>
             <SwiperItem>
-              <View>A</View>
+              <View className='displayNormal'>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>违章查询</Text>
+                </View>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>检测报告</Text>
+                </View>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>维修记录</Text>
+                </View>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>事故记录</Text>
+                </View>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>车辆评估</Text>
+                </View>
+              </View>
+            </SwiperItem>
+            <SwiperItem>
+              <View className='displayNormal'>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>违章查询</Text>
+                </View>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>检测报告</Text>
+                </View>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>维修记录</Text>
+                </View>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>事故记录</Text>
+                </View>
+                <View className='menuItem'>
+                  <Image className='icon' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2534199263,3708083788&fm=11&gp=0.jpg'></Image>
+                  <Text>车辆评估</Text>
+                </View>
+              </View>
             </SwiperItem>
           </Swiper>
+          <View className='swiperBots'>
+            <View className='dot'></View>
+            <View className='dot'></View>
+          </View>
         </View>
       </View>
     )
